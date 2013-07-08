01902fd529ca9e4992cb43e915b23ed09f886749
kegare <kegare0127@gmail.com>
2013-07-09 08:18:37 +0900

    Updated to 1.6.2-v1.0.4

--- givingitem/GivingItem.java
+++ givingitem/GivingItem.java
@@ -1,5 +1,8 @@
 package givingitem;
 
+import java.net.URL;
+import java.util.Properties;
+
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
@@ -7,6 +10,7 @@ import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
@@ -25,12 +29,27 @@ import cpw.mods.fml.common.network.NetworkMod;
 )
 public class GivingItem
 {
-	static final String version = "1.0.3";
+	static final String version = "1.0.4";
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		event.getModMetadata().version = version;
+		try
+		{
+			URL url = new URL("https://dl.dropbox.com/u/51943112/kegare/kegare.info");
+			Properties mappings = new Properties();
+
+			mappings.load(url.openConnection().getInputStream());
+
+			ModMetadata metadata = event.getModMetadata();
+			metadata.version = version;
+			metadata.description += " (Latest: " + mappings.getProperty("givingitem.latest", version) + ")";
+			metadata.url = mappings.getProperty("givingitem.url", metadata.url);
+		}
+		catch (Exception e)
+		{
+			return;
+		}
 	}
 
 	@EventHandler
